466f7cce59ca82b28f5e151352c63e5a530b9d41
brandonTsai <brandonya125@gmail.com>
2022-10-04 07:19:11 -0700

    Correct naming for jobs routes & controller

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import { createJob, deleteJob, getAllJobs, updateJob, showStats } from '../controllers/jobsContriller.js'
+import { createJob, deleteJob, getAllJobs, updateJob, showStats } from '../controllers/jobsController.js'
 
 router.route('/').post(createJob).get(getAllJobs)
 // remember about :id
