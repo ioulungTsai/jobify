a4e33f9b85e16314ac274df5c6870889de37ffdc
brandonTsai <brandonya125@gmail.com>
2022-10-04 06:49:48 -0700

    Setup auth structure for backend

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+const register = (req, res) => {
+  console.log('register user');
+}
+
+const login = (req, res) => {
+  console.log('login user');
+}
+
+const updateUser = (req, res) => {
+  console.log('updateUser');
+}
+
+export {register, login, updateUser}
