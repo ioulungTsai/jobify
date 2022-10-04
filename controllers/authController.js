91de3b618946b492f4d9f4fd8c70b375528934dc
brandonTsai <brandonya125@gmail.com>
2022-10-04 07:57:33 -0700

    Correct some typo to test in Postman

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,13 @@
 const register = async (req, res) => {
-  console.log('register user');
+  res.send('register user');
 }
 
 const login = async (req, res) => {
-  console.log('login user');
+  res.send('login user');
 }
 
 const updateUser = async (req, res) => {
-  console.log('updateUser');
+  res.send('updateUser');
 }
 
 export {register, login, updateUser}
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/job', jobsRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
