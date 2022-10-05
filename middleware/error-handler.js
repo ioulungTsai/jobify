9a114f582cfce3d5c8482969f52acd5804e2c1b7
brandonTsai <brandonya125@gmail.com>
2022-10-04 22:40:19 -0700

    Handle missing field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,9 +4,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: 'Something went wrong, try again later'
+    msg: "Something went wrong, try again later",
+  };
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
   }
-  res.status(defaultError.statusCode).json({msg: err})
-}
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+};
 
-export default errorHandlerMiddleware
+export default errorHandlerMiddleware;
