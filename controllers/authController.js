a6586741e0f11a2c0f414f3d1ba7d5ff92c14f2e
brandonTsai <brandonya125@gmail.com>
2022-10-05 13:47:58 -0700

    Update login logic in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
-import { BadRequestError } from "../errors/index.js"
+import { BadRequestError, UnauthenticatedError } from "../errors/index.js"
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
@@ -30,7 +30,23 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  res.send("login user")
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw BadRequestError("Please provide all values")
+  }
+  const user = await User.findOne({ email }).select("+password")
+  if (!user) {
+    throw UnauthenticatedError("Invalid Credentials")
+  }
+  console.log(user)
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw UnauthenticatedError("Invalid Credentials")
+  }
+  const token = user.createJWT()
+  user.password = undefined
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 const updateUser = async (req, res) => {
