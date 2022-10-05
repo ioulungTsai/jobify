2d7e8d9b92cdd75d8e4cb6fa4440e08a2990d462
brandonTsai <brandonya125@gmail.com>
2022-10-05 10:36:59 -0700

    Update register user

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -25,6 +25,38 @@ const reducer = (state, action) => {
     }
   }
 
+  if(action.type === REGISTER_USER_BIGIN) {
+    return {
+      ...state,
+      isLoadin: true
+    }
+  }
+
+  if(action.type === REGISTER_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoadin: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'User Created! Redirecting...',
+    }
+  }
+
+  if(action.type === REGISTER_USER_ERROR) {
+    return {
+      ...state,
+      isLoadin: false,
+
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    }
+  }
+
   throw Error(`no such action : ${action.type}` )
 }
 
