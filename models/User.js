2b2c93b6bb013ef802b88a1f830db91b6fae3b4a
brandonTsai <brandonya125@gmail.com>
2022-10-04 21:55:16 -0700

    Pass error to error-handler middleware

--- models/User.js
+++ models/User.js
@@ -4,14 +4,14 @@ import validator from "validator";
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "Please provide name"],
+    required: [true, "Please provide name"],
     minlength: 3,
     maxlength: 20,
     trim: true
   },
   email: {
     type: String,
-    require: [true, "Please provide email"],
+    required: [true, "Please provide email"],
     validate:{
       validator: validator.isEmail,
       message: 'Please provide a valid email'
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    require: [true, "Please provide password"],
+    required: [true, "Please provide password"],
     minlength: 6,
   },
   lastName: {
