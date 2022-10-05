020ba80f2a179b1f6c470db193538df0ba1a5025
brandonTsai <brandonya125@gmail.com>
2022-10-04 23:40:49 -0700

    Check for empty values in the controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,22 @@ import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body)
-  res.status(StatusCodes.CREATED).json({user})
-}
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new Error("please provide all values");
+  }
+
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.CREATED).json({ user });
+};
 
 const login = async (req, res) => {
-  res.send('login user');
-}
+  res.send("login user");
+};
 
 const updateUser = async (req, res) => {
-  res.send('updateUser');
-}
+  res.send("updateUser");
+};
 
-export {register, login, updateUser}
+export { register, login, updateUser };
