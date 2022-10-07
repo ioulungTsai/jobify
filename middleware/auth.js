0ef90ca376c1b3752da5b731da771ab48bcef0d8
brandonTsai <brandonya125@gmail.com>
2022-10-07 00:27:57 -0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,19 @@
-const auth = async (req, res, next) => {
-  const headers = req.headers
-  const authHeader = req.headers.authorization
+import jwt from "jsonwebtoken"
+import { UnauthenticatedError } from "../errors/index.js"
 
-  console.log(headers)
-  console.log(authHeader)
-  next()
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("Authentication Invalid")
+  }
+  const token = authHeader.split(" ")[1]
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = { userId: payload.userId }
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication Invalid")
+  }
 }
 
 export default auth
--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ UserSchema.pre("save", async function () {
 })
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ suerId: this._id }, process.env.JWT_SECRET, {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   })
 }
