d47a96056c02d7b4443b4e47e913b74861b391aa
brandonTsai <brandonya125@gmail.com>
2022-10-07 01:09:05 -0700

    Update updateUser in authController and UserSchema.pre in User

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,8 +50,22 @@ const login = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
-  console.log(req.user)
-  res.send("updateUser")
+  const { name, email, lastName, location } = req.body
+  if (!email || !name || !lastName || !location) {
+    throw BadRequestError("Please provide all values")
+  }
+  const user = await User.findOne({ _id: req.user.userId })
+
+  user.name = name
+  user.email = email
+  user.lastName = lastName
+  user.location = location
+
+  await user.save()
+
+  const token = user.createJWT()
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 export { register, login, updateUser }
--- models/User.js
+++ models/User.js
@@ -42,6 +42,7 @@ const UserSchema = new mongoose.Schema({
 
 // use old fashion function declaration due to the "this" binding issue
 UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
