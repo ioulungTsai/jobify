35c7b4d31435c8b3990197b7e330c4bff4a3bb8a
brandonTsai <brandonya125@gmail.com>
2022-10-09 14:47:00 -0700

    Update JobInfo and add into Job

--- client/src/components/JobInfo.js
+++ client/src/components/JobInfo.js
@@ -1,6 +1,11 @@
-const JobInfo = () => {
+import Wrapper from "../assets/wrappers/JobInfo"
+
+const JobInfo = ({ icon, text }) => {
   return (
-    <h2>job info</h2>
+    <Wrapper>
+      <span className="icon">{icon}</span>
+      <span className="text">{text}</span>
+    </Wrapper>
   )
 }
 export default JobInfo
