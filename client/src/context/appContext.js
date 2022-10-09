98e857d4ba06c1980dd6440d5aebdb303c7eb048
brandonTsai <brandonya125@gmail.com>
2022-10-09 11:58:24 -0700

    Implement get all jobs request

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -18,6 +18,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from "./actions"
 
 const token = localStorage.getItem("token")
@@ -42,6 +44,10 @@ const initialSate = {
   jobType: "full-time",
   statusOptions: ["interview", "declined", "pending"],
   status: "pending",
+  jobs: [],
+  totalJobs: 0,
+  numOfPage: 1,
+  page: 1,
 }
 
 const AppContext = React.createContext()
@@ -188,6 +194,27 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const getJobs = async () => {
+    let url = `/jobs`
+
+    dispatch({ type: GET_JOBS_BEGIN })
+    try {
+      const { data } = await authFetch(url)
+      const { jobs, totalJobs, numOfPage } = data
+      dispatch({
+        type: GET_JOBS_SUCCESS,
+        payload: {
+          jobs,
+          totalJobs,
+          numOfPage,
+        },
+      })
+    } catch (error) {
+      console.log(error.reponse)
+    }
+    clearAlert()
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -200,6 +227,7 @@ const AppProvider = ({ children }) => {
         handleChange,
         clearValues,
         createJob,
+        getJobs,
       }}
     >
       {children}
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -14,6 +14,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from "./actions"
 
 import { initialSate } from "./appContext"
@@ -166,6 +168,20 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: "danger",
       alertText: action.payload.msg,
+    }
+  }
+
+  if (action.type === GET_JOBS_BEGIN) {
+    return { ...state, isLoadin: true, showAlert: false }
+  }
+
+  if (action.type === GET_JOBS_SUCCESS) {
+    return {
+      ...state,
+      isLoadin: false,
+      jobs: action.payload.jobs,
+      totalJobs: action.payload.totalJobs,
+      numOfPages: action.payload.numOfPages,
     }
   }
 
