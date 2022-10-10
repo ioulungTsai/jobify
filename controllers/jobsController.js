6d0e47478860a2722b89784b344bb29929ec3c61
brandonTsai <brandonya125@gmail.com>
2022-10-09 23:23:25 -0700

    Create checkPermission

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,11 @@
 import Job from "../models/Job.js"
 import { StatusCodes } from "http-status-codes"
-import { BadRequestError, UnauthenticatedError } from "../errors/index.js"
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} from "../errors/index.js"
+import checkPermissions from "../utils/checkPermissions.js"
 
 const createJob = async (req, res) => {
   const { position, company } = req.body
@@ -25,7 +30,26 @@ const deleteJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-  res.send("update job")
+  const { id: jobId } = req.params
+  const { company, position } = req.body
+
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values")
+  }
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`)
+  }
+
+  // check permissions
+  checkPermissions(req.user, job.createdBy)
+
+  const updateJob = await Job.findByIdAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+  res.status(StatusCodes.OK).json({ updateJob })
 }
 
 const showStats = async (req, res) => {
