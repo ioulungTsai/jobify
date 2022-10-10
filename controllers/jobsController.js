83c02f9551fbba9b308be43b9f50d2cad141fd89
brandonTsai <brandonya125@gmail.com>
2022-10-09 23:39:40 -0700

    Update deleteJob in JobsController

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -25,10 +25,6 @@ const getAllJobs = async (req, res) => {
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
 }
 
-const deleteJob = async (req, res) => {
-  res.send("delete job")
-}
-
 const updateJob = async (req, res) => {
   const { id: jobId } = req.params
   const { company, position } = req.body
@@ -50,6 +46,21 @@ const updateJob = async (req, res) => {
     runValidators: true,
   })
   res.status(StatusCodes.OK).json({ updateJob })
+}
+
+const deleteJob = async (req, res) => {
+  const { id: jobId } = req.params
+
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`)
+  }
+
+  checkPermissions(req.user, job.createdBy)
+
+  await job.remove()
+  res.status(StatusCodes.OK).json({ msg: "Success! Job removed" })
 }
 
 const showStats = async (req, res) => {
