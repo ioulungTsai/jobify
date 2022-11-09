c00304a65e550c6627605a66d4efb649fd2ae00c
brandonTsai <brandonya125@gmail.com>
2022-11-09 01:10:17 -0800

    Add debounce in the job search input

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,11 +1,13 @@
 import { FormRow, FormRowSelect } from "."
 import { useAppContext } from "../context/appContext"
 import Wrapper from "../assets/wrappers/SearchContainer"
+import { useState, useMemo } from "react"
 
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState("")
   const {
     isLoading,
-    search,
+    // search,
     searchStatus,
     searchType,
     sort,
@@ -17,7 +19,6 @@ const SearchContainer = () => {
   } = useAppContext()
 
   const handleSearch = (e) => {
-    if (isLoading) return
     handleChange({ name: e.target.name, value: e.target.value })
   }
 
@@ -25,6 +26,19 @@ const SearchContainer = () => {
     e.preventDefault()
     clearFilters()
   }
+
+  const debounce = () => {
+    let timeoutID
+    return (e) => {
+      setLocalSearch(e.target.value)
+      clearTimeout(timeoutID)
+      timeoutID = setTimeout(() => {
+        handleChange({ name: e.target.name, value: e.target.value })
+      }, 1000)
+    }
+  }
+  // eslint-disable-next-line
+  const optimizedDebounce = useMemo(() => debounce(), [])
 
   return (
     <Wrapper>
@@ -35,8 +49,8 @@ const SearchContainer = () => {
           <FormRow
             type="text"
             name="search"
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           {/* search by status */}
           <FormRowSelect
